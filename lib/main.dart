2966acb5c5b231a3aad86da5365412558f834797
Brian Richard Kleis <brian.kleis@gmail.com>
2021-09-09 08:09:38 -0300

    realizado a criação do cartão de visitas

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red.shade100,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/brian.jpg'),
+              ),
+              Text(
+                'Brian Richard Kleis',
+                style: TextStyle(
+                  fontSize: 35.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'DEVOPS',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.red.shade200,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.red.shade200,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.red.shade300,
+                  ),
+                  title: Text(
+                    '+55 47 98472-2128',
+                    style: TextStyle(
+                      color: Colors.red.shade300,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red.shade300,
+                  ),
+                  title: Text(
+                    'brian.kleis@catolicasc.edu.br',
+                    style: TextStyle(
+                      color: Colors.red.shade300,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
